7df5609c887d7bc71f290bf4296e957d0cf64a85
haroldocurti <haroldocurti@gmail.com>
2022-10-20 13:52:35 -0300

    Troca de estrutura + implementação de subtração.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const populatebt = document.getElementById("populate");
 const baseSelector = document.getElementById("base-select");
+const opSelect = document.getElementById("op-select");
 const spanBoard = document.getElementById("spanBoard");
 // array para os códigos HTML de Subscript e iteração para popular o vetor.
 const baseHTML = [];
@@ -30,8 +31,46 @@ function calcSoma(base) {
     i++;
   }
 }
+function calcSub(base) {
+  spanBoard.innerHTML = ""; //limpa
+  let i = 0;
+  while (i < 10) {
+    const subtraction = {
+      n1: Math.floor(Math.random() * 2000),
+      n2: Math.floor(Math.random() * 2000),
+    };
+    if (subtraction.n1 < subtraction.n2) {
+      let sub = subtraction.n2 - subtraction.n1;
+      subtraction.result = sub.toString(base);
+      const newOp = document.createElement("details");
+      newOp.innerHTML = `<summary>${subtraction.n2.toString(base)}${
+        baseHTML[base]
+      } - ${subtraction.n1.toString(base)}${baseHTML[base]}</summary> <p>${
+        subtraction.result
+      }${baseHTML[base]}</details></p>`;
+      spanBoard.appendChild(newOp);
+      i++;
+    } else {
+      let sub = subtraction.n1 - subtraction.n2;
+      subtraction.result = sub.toString(base);
+      const newOp = document.createElement("details");
+      newOp.innerHTML = `<summary>${subtraction.n1.toString(base)}${
+        baseHTML[base]
+      } - ${subtraction.n2.toString(base)}${baseHTML[base]}</summary> <p>${
+        subtraction.result
+      }${baseHTML[base]}</details></p>`;
+      spanBoard.appendChild(newOp);
+      i++;
+    }
+  }
+}
 populatebt.addEventListener("click", function () {
-  base = parseInt(baseSelector.value);
-
-  calcSoma(base);
+  let base = parseInt(baseSelector.value);
+  let operacao = opSelect.value;
+  if (operacao == "soma") {
+    calcSoma(base);
+  }
+  if (operacao == "subtracao") {
+    calcSub(base);
+  }
 });
